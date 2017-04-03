272d5546aa3e5e69e1edafb05e0535c86e5e99b3
Unknown <asasinn.ru@inbox.ru>
2017-04-03 17:45:37 +0300

    первый коммит

---
+++ controllers/TeachersController.php
@@ -0,0 +1,28 @@
+<?php
+namespace app\controllers;
+
+use yii\web\Controller;
+
+
+class TeachersController extends Controller
+{
+    public function actionIndex()
+    {
+       return $this->render('index');
+    }
+
+    public function actionCreate()
+    {
+
+    }
+
+    public function actionEdit()
+    {
+
+    }
+
+    public function actionDelete()
+    {
+
+    }
+}
