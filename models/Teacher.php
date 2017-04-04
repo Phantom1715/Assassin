2a27179544c627fd2a29946076f1dce545fbd2dd
Unknown <asasinn.ru@inbox.ru>
2017-04-04 17:46:07 +0300

    Начал Контроллер,модель ,учитель

---
+++ models/Teacher.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\models;
+
+class Teacher extends \yii\db\ActiveRecord {
+    public static function tableName() {
+        return 'Teacher';
+    }
+
+    public function  rules() {
+        return [
+            [['name','name_f','name_ot'],'required'],
+            [['name','name_f','name_ot'],'string','max' => 100],
+            [['birthday','employment'],'safe']
+        ];
+    }
+
+    public function attributeLabels() {
+        return [
+            'name' => 'Name',
+            'name_f' => 'Full_name',
+            'name_ot' => 'Patronymic',
+            'birthday' => 'Birthday',
+            'employment' => 'Employment'
+        ];
+    }
+}
